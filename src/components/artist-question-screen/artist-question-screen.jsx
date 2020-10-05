1c375e29fe64e7438a0ef21c85bab92008a76ba1
Alena Shashko <L.Shashko@mail.ru>
2020-10-05 17:49:42 +0300

    fix bug with artist picture's placeholder

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -36,7 +36,7 @@ const ArtistQuestionScreen = () => {
           <div className="artist">
             <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-1" id="answer-1" />
             <label className="artist__name" htmlFor="answer-1">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Пелагея" />
+              <img className="artist__picture" src="/img/placeholder.jpg" alt="Пелагея" />
               Пелагея
             </label>
           </div>
@@ -44,7 +44,7 @@ const ArtistQuestionScreen = () => {
           <div className="artist">
             <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-2" id="answer-2" />
             <label className="artist__name" htmlFor="answer-2">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Пелагея" />
+              <img className="artist__picture" src="/img/placeholder.jpg" alt="Пелагея" />
               Краснознаменная дивизия имени моей бабушки
             </label>
           </div>
@@ -52,7 +52,7 @@ const ArtistQuestionScreen = () => {
           <div className="artist">
             <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-3" id="answer-3" />
             <label className="artist__name" htmlFor="answer-3">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Пелагея" />
+              <img className="artist__picture" src="/img/placeholder.jpg" alt="Пелагея" />
               Lordi
             </label>
           </div>
