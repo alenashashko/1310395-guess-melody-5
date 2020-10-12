cd82805f7c4fd2354b765513c0e747d0bb7a83c3
Alena Shashko <L.Shashko@mail.ru>
2020-10-12 23:55:47 +0300

    create onPlayButtonClick callback

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,34 +3,45 @@ import PropTypes from 'prop-types';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 import AuthScreen from '../auth-screen/auth-screen';
-import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
-import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+// import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+// import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import GameScreen from '../game-screen/game-screen';
 import GameOverScreen from '../game-over-screen/game-over-screen';
 import WinScreen from '../win-screen/win-screen';
 
 const App = (props) => {
-  const {errorsCount} = props;
+  const {errorsCount, questions} = props;
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/'>
-          <WelcomeScreen errorsCount={errorsCount} />
+        <Route exact path='/' render={(routeProps) => {
+          const {history} = routeProps;
+          return (
+            <WelcomeScreen
+              errorsCount={errorsCount}
+              onPlayButtonClick={() => history.push(`/game`)}
+            />
+          );
+        }}>
         </Route>
         <Route exact path='/login'>
           <AuthScreen />
         </Route>
-        <Route exact path='/dev-artist'>
+        {/* <Route exact path='/dev-artist'>
           <ArtistQuestionScreen />
         </Route>
         <Route exact path='/dev-genre'>
           <GenreQuestionScreen />
-        </Route>
+        </Route> */}
         <Route exact path='/lose'>
           <GameOverScreen />
         </Route>
         <Route exact path='/result'>
           <WinScreen />
+        </Route>
+        <Route exact path='/game'>
+          <GameScreen questions={questions}/>
         </Route>
       </Switch>
     </BrowserRouter>
@@ -39,6 +50,7 @@ const App = (props) => {
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired
 };
 
 export default App;
