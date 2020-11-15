37c26fdb22dbe427e6703f3d9dd1c50e0354b1aa
Alena Shashko <L.Shashko@mail.ru>
2020-11-16 01:52:11 +0300

    add incrementMistake action creator

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,10 @@
+import {GameType} from '../const';
+import {isArtistAnswerCorrect, isGenreAnswerCorrect} from '../game';
+
 export const ActionType = {
-  INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
   INCREMENT_STEP: `INCREMENT_STEP`,
-  RESET_GAME: `RESET_GAME`
+  RESET_GAME: `RESET_GAME`,
+  INCREMENT_MISTAKES: `INCREMENT_MISTAKES`
 };
 
 export const ActionCreator = {
@@ -11,5 +14,22 @@ export const ActionCreator = {
   }),
   resetGame: () => ({
     type: ActionType.RESET_GAME
-  })
+  }),
+  incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect = false;
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+    }
+
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ? 0 : 1 // ?
+    };
+  }
 };
