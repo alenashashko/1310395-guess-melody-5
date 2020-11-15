1c0249efa6e9802349888628aa9fe3e9455219fa
Alena Shashko <L.Shashko@mail.ru>
2020-11-16 00:20:18 +0300

    create actions and reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,24 @@
+import {ActionType} from './actions';
+import {extend} from '../utils';
+
+const initialState = {
+  mistakes: 0,
+  step: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.INCREMENT_MISTAKES:
+      return extend(state, {
+        mistakes: state.mistakes + action.payload // ?
+      });
+    case ActionType.INCREMENT_STEP:
+      return extend(state, {
+        step: state.step + action.payload // ?
+      });
+    default:
+      return state;
+  }
+};
+
+export {reducer}; // ?
