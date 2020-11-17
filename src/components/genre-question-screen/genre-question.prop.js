3225e08e85cc9871c2180f81fb7b33828ad33bb3
Alena Shashko <L.Shashko@mail.ru>
2020-11-17 18:02:39 +0300

    create with-user-answer hocs

--- src/components/genre-question-screen/genre-question.prop.js
+++ src/components/genre-question-screen/genre-question.prop.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import {GameType} from '../../const';
 
 export default PropTypes.shape({
-  type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
+  type: PropTypes.oneOf([GameType.GENRE]).isRequired,
   genre: PropTypes.string.isRequired,
   answers: PropTypes.arrayOf(PropTypes.shape({
     src: PropTypes.string.isRequired,
